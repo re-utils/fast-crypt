c9b4ce0c9e8116331e7d26760d7c593a241586bc
aquapi <aquaplmc@gmail.com>
2025-04-03 23:46:43 +0700

    Initial commit (via bun create)

---
+++ scripts/report-size.ts
@@ -0,0 +1,44 @@
+import { minify_sync } from 'terser';
+import { LIB } from './utils.js';
+
+const sizes: {
+  entry: string,
+  size: number,
+  minified: number,
+  gzip: number,
+  minifiedGzip: number,
+}[] = [];
+
+const toByte = (num: number) =>
+  num >= 1e3
+    ? (num / 1e3).toFixed(2) + 'KB'
+    : num + 'B';
+
+for await (const path of new Bun.Glob('**/*.mjs').scan(LIB)) {
+  const file = Bun.file(LIB + '/' + path);
+  const code = await file.text();
+  const minfiedCode = minify_sync(code).code!;
+
+  sizes.push({
+    entry: path,
+    size: file.size,
+    minified: Buffer.from(minfiedCode).byteLength,
+    gzip: Bun.gzipSync(code).byteLength,
+    minifiedGzip: Bun.gzipSync(minfiedCode).byteLength
+  });
+}
+
+sizes.sort((a, b) => a.size - b.size);
+
+// Convert to table columns
+console.table(
+  sizes.map(
+    (val) => ({
+      Entry: val.entry,
+      Size: toByte(val.size),
+      Minify: toByte(val.minified),
+      GZIP: toByte(val.gzip),
+      'Minify GZIP': toByte(val.minifiedGzip)
+    })
+  )
+);
---
+++ scripts/utils.ts
@@ -0,0 +1,12 @@
+import { write, file, $, type ShellOutput } from 'bun';
+import { resolve, join } from 'node:path/posix';
+
+export const SCRIPTS = import.meta.dir;
+export const ROOT = resolve(SCRIPTS, '..');
+export const SOURCE = ROOT + '/src';
+export const LIB = ROOT + '/lib';
+export const BENCH = ROOT + '/bench';
+
+export const cp = (from: string, to: string, path: string) => write(join(to, path), file(join(from, path)));
+export const exec = (...args: Parameters<typeof $>) => $(...args).catch((err: ShellOutput) => process.stderr.write(err.stderr as any));
+export const cd = (dir: string) => $.cwd(dir);
