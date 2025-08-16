11e27bf52cb811fcacf7d542bdd2c9214f11f669
aquapi <aquaplmc@gmail.com>
2025-08-16 15:08:16 +0700

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * as cookie from './cookie.ts';
 export * as hmac from './hmac.ts';
 export * as scrypt from './scrypt.ts';
+export * as secrets from './secrets.ts';
 export * as utils from './utils.ts';
--- tests/scrypt.test.ts
+++ tests/scrypt.test.ts
@@ -1,12 +1,11 @@
 import { expect, test } from 'bun:test';
-import * as st from '@safe-std/error';
 import * as scrypt from '../src/scrypt.ts';
 
 test('Basic signer', async () => {
   const sign = scrypt.signer();
   const hash = await sign('Hi');
 
-  expect(st.isErr(hash), 'Hashing failed').toBeFalse();
+  expect(hash, 'Hashing failed').toBeTypeOf('string');
   expect(
     await scrypt.verify('Hi', hash as string),
     'Hash verification failed',
